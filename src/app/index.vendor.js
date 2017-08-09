fbd413a1cb6efde248dfa468a4f5e057a0d532bf
Christine Lee <cylee4@gmail.com>
2017-08-08 20:44:33 -0700

    App generator setup

---
+++ src/app/index.vendor.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// node_modules
+import "angular";
+
+import "angular-ui-router";
+
+
+import "oclazyload";
+
+import "angular-sanitize";
+
+import "jquery";
+
+
+
+// local scripts
+//import "../assets/js/...";
