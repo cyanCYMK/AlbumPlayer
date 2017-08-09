6743707fa5bd4205c379269970f8f81456e73ca4
Christine Lee <cylee4@gmail.com>
2017-08-09 07:54:16 -0700

    Play/pause in styled element (WIP)

--- src/app/components/player/player.component.js
+++ src/app/components/player/player.component.js
@@ -5,14 +5,17 @@ import templateUrl from './player.html';
 export const PlayerComponent = {
   templateUrl,
   controller: class PlayerComponent {
-    constructor(PlayerService, $sce) {
+    constructor(PlayerService, $sce, $element, $scope) {
       'ngInject';
       this.$sce = $sce;
+      this.$element = $element;
+      this.$scope = $scope;
       this.playerService = PlayerService;
     }
 
     $onInit() {
       this.loading = true;
+      this.playing = false;
       this.playerService.getAlbum().then(({album_name, artist, tracks}) => {
         this.loading = false;
         this.tracks = tracks;
@@ -20,6 +23,8 @@ export const PlayerComponent = {
         this.artist = artist;
 
         // by default set current track to first track
+        // TODO: separate this into $postLink and set initial track value there
+        // TODO: separate audio into stateless component
         this.currentTrack = this._initializeCurrentTrack(this.tracks[0], 0);
         console.log(this.currentTrack)
       });
@@ -30,7 +35,15 @@ export const PlayerComponent = {
       if (index < 0 || index > this.tracks.length - 1) return;
       this.currentTrack = this._initializeCurrentTrack(this.tracks[index], index);
       console.log(this.currentTrack)
+    }
 
+    togglePlay() {
+      // access element and call .play() or .pause()
+      this.playing = !this.playing;
+      let audio = this.$element.find('audio')[0];
+      if (audio) {
+        this.playing === true ? audio.play() : audio.pause();
+      }
     }
 
     _initializeCurrentTrack(track, index) {
