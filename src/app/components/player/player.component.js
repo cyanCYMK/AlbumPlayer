235f764d059a9b7558d46171772acf3b7f68977b
Christine Lee <cylee4@gmail.com>
2017-08-12 18:44:58 -0700

    Cache Howl sound with tracks, split play/pause into separate buttons/methods

--- src/app/components/player/player.component.js
+++ src/app/components/player/player.component.js
@@ -19,6 +19,7 @@ export const PlayerComponent = {
       this.loading = true;
       this.playing = false;
       this.playerService.getAlbum().then(({album_name, artist, tracks}) => {
+        console.log('got album:', album_name)
         this.loading = false;
         this.tracks = tracks;
         this.albumName = album_name;
@@ -27,64 +28,58 @@ export const PlayerComponent = {
         // by default set current track to first track
         // TODO: separate this into $postLink and set initial track value there
         // TODO: separate audio controls into stateless component
-        this.currentTrack = this._initializeCurrentTrack(this.tracks[0], 0);
-
-console.log('src:', this.currentTrack.url)
-        this.sound = new Howl({
-          src: [this.currentTrack.url],
-          onplay: function() {
-             // Display the duration.
-             duration.innerHTML = self.formatTime(Math.round(sound.duration()));
-
-             // Start upating the progress of the track.
-             requestAnimationFrame(self.step.bind(self));
-
-             // Start the wave animation if we have already loaded
-             wave.container.style.display = 'block';
-             bar.style.display = 'none';
-             pauseBtn.style.display = 'block';
-           },
-           onload: function() {
-             // Start the wave animation.
-             console.log('song loaded')
-             console.log('duration?', this.duration)
-           },
-           onend: function() {
-             // Stop the wave animation.
-             console.log('song ended')
-           },
-           onpause: function() {
-             // Stop the wave animation.
-             console.log('paused')
-           },
-        });
+        this.index = 0;
+        this.tracks[0] = this._initializeCurrentTrack(this.tracks[0], 0);
       });
     }
 
-    changeTrack(index) {
-      // changes current track to index specified
-      if (index < 0 || index > this.tracks.length - 1) return;
-      this.currentTrack = this._initializeCurrentTrack(this.tracks[index], index);
-      // don't play audio
-      this.togglePlay(false);
-      console.log(this.currentTrack)
-      // get audio element and find attributes
-      console.log('duration:', this.$element.find('audio').attr('duration'))
+    step() {
+      // called with requestAnimationFrame to update playback position
+      const currentTrack = this.tracks[this.index];
+      let seek = currentTrack.sound.seek() || 0;
+      let duration = currentTrack.sound.duration();
+      this.timer = this._formatTime(Math.round(seek));
+      this.progressWidth = (((seek / currentTrack.sound.duration()) * 100) || 0) + '%';
+      this.elapsed = this.timer;
+      this.remaining = this._formatTime(Math.round(duration - seek));
+
+      // If the sound is still playing, continue stepping.
+      if (!currentTrack.sound.playing()) {
+        this.$interval.cancel(this.timerInterval);
+      }
     }
 
-    togglePlay(playing) {
-      // access element and call .play() or .pause()
-      // if playing specified, play or pause audio; otherwise toggle
-      if (playing) {
-        this.playing = playing;
-      } else {
-        this.playing = !this.playing;
+    changeTrack(index) {
+      if (index < 0 || index > this.tracks.length - 1) return;
+
+      console.log('stopping track:', this.index)
+      console.log('changing track to:', index)
+
+      const previousTrack = this.tracks[this.index];
+      previousTrack.sound.stop();
+
+      // change state for intervals and index
+      // this.$interval.cancel(this.timerInterval);
+      this.index = index;
+
+      if (!this.tracks[index].sound) {
+        this.tracks[index] = this._initializeCurrentTrack(this.tracks[index], index);
       }
-      let audio = this.$element.find('audio')[0];
-      if (audio) {
-        console.log('playing sound:', this.sound)
-        this.playing === true ? this.sound.play() : this.sound.pause();
-      }
+
+      // for views
+      this.playing = false; // for viewing pause/play button
+      this.remaining = this._formatTime(Math.round(this.tracks[this.index].sound.duration()));
+      this.elapsed = '';
+    }
+
+    pause() {
+      this.playing = false;
+      this.tracks[this.index].sound.pause();
+    }
+
+    play() {
+      this.playing = true;
+      this.tracks[this.index].sound.play();
     }
 
     checkPlayTime(playing) {
@@ -95,14 +90,50 @@ console.log('src:', this.currentTrack.url)
 
     _initializeCurrentTrack(track, index) {
       // helper function to properties of track for rendering
+      const sound = new Howl({
+        src: [track.url],
+        onplay: () => {
+          // Start upating the progress of the track.
+          this.timerInterval = this.$interval(this.step.bind(this), 1000);
+         },
+         onload: () => {
+           // Set remaining time.
+           console.log('song loaded')
+           this.$scope.$apply(() => {
+             this.remaining = this._formatTime(Math.round(this.tracks[this.index].sound.duration()));
+           });
+         },
+         onend: () => {
+           // Stop the wave animation.
+           console.log('song ended, canceling interval')
+           this.$interval.cancel(this.timerInterval);
+
+         },
+         onpause: () => {
+           // Stop the wave animation.
+           console.log('paused, canceling interval')
+           this.$interval.cancel(this.timerInterval);
+         },
+         onstop: () => {
+           console.log('stopped, canceling interval')
+           this.$interval.cancel(this.timerInterval);
+         }
+      });
+
       return {
         ...track,
         index,
-        // url: this.$sce.trustAsResourceUrl(track.url),
+        sound,
         style: {
           'background-image': `url('${track.cover_image}')`,
         }
       }
+    }
+    _formatTime(secs) {
+      const minutes = Math.floor(secs / 60) || 0;
+      const seconds = (secs - minutes * 60) || 0;
+
+      return `${minutes}:${(seconds < 10 ? 0 : '')}${seconds}`;
     }
   }
 };
